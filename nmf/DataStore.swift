078a4790f9c00cef0d9842756e0bfbdb7312feac
Ben Lachman <ben@nicemohawk.com>
2016-04-25 10:05:31 -0700

    Updated data store
    Update method now take a completion so that we know when they are
    finished.

--- nmf/DataStore.swift
+++ nmf/DataStore.swift
@@ -10,44 +10,67 @@ import Foundation
 
 private let dataStoreSingleton = DataStore()
 
+extension Fault : ErrorType {
+    
+}
+
 class DataStore: NSObject {
     var scheduleItems: BackendlessCollection?
-    var artistsItems: Artists?
+    var artistsItems: [Artists]?
     
     
     class var sharedInstance: DataStore {
         return dataStoreSingleton
     }
     
-    override init() {
-    
-        super.init()
-        self.updateScheduleItems()
-        
-        }
-    func updateScheduleItems() -> Void {
-        
+    func updateScheduleItems(completion: (ErrorType?) -> Void) -> Void {
         let backendless = Backendless.sharedInstance()
         let dataStore = backendless.data.of(Schedule.ofClass())
+        
         dataStore.find({ (scheduleItemsCollection) in
             self.scheduleItems = scheduleItemsCollection
+            
+            completion(nil)
         }) { (fault) in
             self.scheduleItems = nil
             print(fault)
+            
+            completion(fault)
         }
-        
     }
-    func updateArtistsItems(artistName: String) -> Void {
+    
+    func updateArtistsItems(artistName: String, completion: (ErrorType?) -> Void) -> Void {
         let backendless = Backendless.sharedInstance()
         let dataStore = backendless.data.of(Artists.ofClass())
-        let artistsQuery = BackendlessDataQuery()
-        artistsQuery.whereClause = "ArtistName = '\(artistName)'"
-        dataStore.find(artistsQuery, response: { (artistsItemsCollection) in
-            self.artistsItems = artistsItemsCollection.data[0] as? Artists
-            }) { (fault) in
-                self.artistsItems = nil
-                print(fault)
+        
+        dataStore.find({ (artistsItemsCollection) in
+            self.artistsItems = artistsItemsCollection.data as? [Artists]
+            
+            completion(nil)
+        }) { (fault) in
+            self.artistsItems = nil
+            print(fault)
+            
+            completion(fault)
         }
     }
-
+    
+    func getArtistByName(artistName: String, completion: (Artists?, ErrorType?) -> Void) -> Void {
+        let backendless = Backendless.sharedInstance()
+        let dataStore = backendless.data.of(Artists.ofClass())
+        
+        let artistsQuery = BackendlessDataQuery()
+        artistsQuery.whereClause = "ArtistName = '\(artistName)'"
+        
+        dataStore.find(artistsQuery, response: { (artistsItemsCollection) in
+            let foundArtist = artistsItemsCollection.data.first as? Artists
+            
+            completion(foundArtist, nil)
+        }) { (fault) in
+            self.artistsItems = nil
+            print(fault)
+            
+            completion(nil, fault)
+        }
+    }
 }
